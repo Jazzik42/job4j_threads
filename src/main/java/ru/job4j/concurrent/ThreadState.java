c8a9049f71cc01724e267bbe13c200d9146ac6dc
Jazzik42 <rmuzrrr@gmail.com>
2021-08-12 22:35:35 +0500

    1.1. Состояние нити. [#229175]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -12,6 +12,8 @@ public class ThreadState {
                     System.out.println(Thread.currentThread().getName());
                 }
         );
+        System.out.println(first.getState());
+        System.out.println(second.getState());
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED
